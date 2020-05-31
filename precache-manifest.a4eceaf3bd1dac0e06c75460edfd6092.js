a7dcd3760f5f3cb1236149250306bf4aaa5a2eb4
pyaenaing <43054337+pyaenaing@users.noreply.github.com>
2020-05-31 00:31:45 -0700

    Updates

---
+++ precache-manifest.a4eceaf3bd1dac0e06c75460edfd6092.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5130a48c977f3ebea8577be60f37e75a",
+    "url": "/Note-App/index.html"
+  },
+  {
+    "revision": "5ca022ee957fc3af13be",
+    "url": "/Note-App/static/css/main.66d80578.chunk.css"
+  },
+  {
+    "revision": "c2011584ae76c7bf2222",
+    "url": "/Note-App/static/js/2.a327b284.chunk.js"
+  },
+  {
+    "revision": "5ca022ee957fc3af13be",
+    "url": "/Note-App/static/js/main.d178fed1.chunk.js"
+  },
+  {
+    "revision": "bd65de24ec0c68e1246d",
+    "url": "/Note-App/static/js/runtime-main.d36590b3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6e7f82820bcb548a0b5c7d622b1a95aa.js"
+  "/Note-App/precache-manifest.a4eceaf3bd1dac0e06c75460edfd6092.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Note-App/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
